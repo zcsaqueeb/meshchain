34d2bdcbd1ddcdde3e117a746442985d31a18fa5
vonssy <kak.reyvaldizakaria@gmail.com>
2024-12-03 20:35:52 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# MeshChain BOT
+MeshChain BOT
+
+Register Here : [MeshChain](https://t.me/meshchain_bot?start=T_1493482017)
+
+## Fitur
+
+  - Auto Get Account Information
+  - Auto Start & Claim Mining Node
+  - Auto Claim Refferal Reward
+  - Auto Complete Tasks
+  - Multi Account
+
+## Prasyarat
+
+Pastikan Anda telah menginstal Python3.9 dan PIP.
+
+## Instalasi
+
+1. **Kloning repositori:**
+   ```bash
+   git clone https://github.com/vonssy/MeshChain-BOT.git
+   ```
+   ```bash
+   cd MeshChain-BOT
+   ```
+
+2. **Instal Requirements:**
+   ```bash
+   pip install -r requirements.txt #or pip3 install -r requirements.txt
+   ```
+
+## Konfigurasi
+
+- **query.txt:** Anda akan menemukan file `query.txt` di dalam direktori proyek. Pastikan `query.txt` berisi data yang sesuai dengan format yang diharapkan oleh skrip. Berikut adalah contoh format file:
+
+  ```bash
+  query_id=
+  user=
+  ```
+
+## Jalankan
+
+```bash
+python bot.py #or python3 bot.py
+```
+
+## Penutup
+
+Terima kasih telah mengunjungi repository ini, jangan lupa untuk memberikan kontribusi berupa follow dan stars.
+Jika Anda memiliki pertanyaan, menemukan masalah, atau memiliki saran untuk perbaikan, jangan ragu untuk menghubungi saya atau membuka *issue* di repositori GitHub ini.
+
+**vonssy**
---
+++ bot.py
@@ -0,0 +1,432 @@
+import cloudscraper
+import json
+import os
+import urllib.parse
+from colorama import *
+from datetime import datetime, timedelta, timezone
+import time
+import pytz
+
+wib = pytz.timezone('Asia/Jakarta')
+
+class MeshChain:
+    def __init__(self) -> None:
+        self.scraper = cloudscraper.create_scraper()
+        self.headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Cache-Control': 'no-cache',
+            'Host': 'api.meshchain.ai',
+            'Origin': 'https://miniapp.meshchain.ai',
+            'Pragma': 'no-cache',
+            'Referer': 'https://miniapp.meshchain.ai/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0'
+        }
+
+    def clear_terminal(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def log(self, message):
+        print(
+            f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}{message}",
+            flush=True
+        )
+
+    def welcome(self):
+        print(
+            f"""
+        {Fore.GREEN + Style.BRIGHT}Auto Claim {Fore.BLUE + Style.BRIGHT}MeshChain - BOT
+            """
+            f"""
+        {Fore.GREEN + Style.BRIGHT}Rey? {Fore.YELLOW + Style.BRIGHT}<INI WATERMARK>
+            """
+        )
+
+    def format_seconds(self, seconds):
+        hours, remainder = divmod(seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
+    
+    def load_data(self, query: str):
+        query_params = urllib.parse.parse_qs(query)
+        query = query_params.get('user', [None])[0]
+
+        if query:
+            user_data_json = urllib.parse.unquote(query)
+            user_data = json.loads(user_data_json)
+            user_id = str(user_data['id'])
+            return user_id
+        else:
+            raise ValueError("User data not found in query.")
+    
+    def user_signin(self, query: str, user_id: str):
+        url = 'https://api.meshchain.ai/meshmain/auth/telegram-miniapp-signin'
+        data = json.dumps({"referral_code":"T_1493482017"})
+        headers = {
+            **self.headers,
+            'Authorization': f'tma {query}',
+            'Content-Type': 'application/json'
+        }
+
+        response = self.scraper.post(url, headers=headers, data=data)
+        if response.status_code == 400:
+            self.log(
+                f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                f"{Fore.WHITE + Style.BRIGHT} {user_id} {Style.RESET_ALL}"
+                f"{Fore.RED + Style.BRIGHT}Query Id Isn't Valid.{Style.RESET_ALL}"
+                f"{Fore.YELLOW + Style.BRIGHT} Update First {Style.RESET_ALL}"
+                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+            )
+            return
+        elif response.status_code == 403:
+            self.log(
+                f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                f"{Fore.WHITE + Style.BRIGHT} {user_id} {Style.RESET_ALL}"
+                f"{Fore.RED + Style.BRIGHT}Blocked By Cloudflare.{Style.RESET_ALL}"
+                f"{Fore.YELLOW + Style.BRIGHT} Restart Again {Style.RESET_ALL}"
+                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+            )
+            return
+        
+        response.raise_for_status()
+        return response.json()["access_token"]
+    
+    def user_profile(self, token: str):
+        url = 'https://api.meshchain.ai/meshmain/user/profile'
+        headers = {
+            **self.headers,
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = self.scraper.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()
+
+    def user_balance(self, token: str):
+        url = 'https://api.meshchain.ai/meshmain/wallet/tokens'
+        headers = {
+            **self.headers,
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = self.scraper.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()['data']
+
+    def node_status(self, token: str, user_id: str):
+        url = 'https://api.meshchain.ai/meshmain/nodes/status'
+        data = json.dumps({"unique_id":user_id})
+        headers = {
+            **self.headers,
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = self.scraper.post(url, headers=headers, data=data)
+        response.raise_for_status()
+        return response.json()
+
+    def node_start(self, token: str, user_id: str):
+        url = 'https://api.meshchain.ai/meshmain/rewards/start'
+        data = json.dumps({"unique_id":user_id})
+        headers = {
+            **self.headers,
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = self.scraper.post(url, headers=headers, data=data)
+        response.raise_for_status()
+        return response.json()
+
+    def node_claim(self, token: str, user_id: str):
+        url = 'https://api.meshchain.ai/meshmain/rewards/claim'
+        data = json.dumps({"unique_id":user_id})
+        headers = {
+            **self.headers,
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = self.scraper.post(url, headers=headers, data=data)
+        response.raise_for_status()
+        return response.json()
+
+    def refferal_info(self, token: str):
+        url = 'https://api.meshchain.ai/meshmain/referral/info'
+        headers = {
+            **self.headers,
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = self.scraper.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()
+
+    def refferal_claim(self, token: str):
+        url = 'https://api.meshchain.ai/meshmain/referral/claim'
+        data = {}
+        headers = {
+            **self.headers,
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = self.scraper.post(url, headers=headers, json=data)
+        response.raise_for_status()
+        return response.json()
+
+    def mission_lists(self, token: str):
+        url = 'https://api.meshchain.ai/meshmain/mission'
+        headers = {
+            **self.headers,
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = self.scraper.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()
+
+    def mission_claim(self, token: str, mission_id: str):
+        url = 'https://api.meshchain.ai/meshmain/mission/claim'
+        data = json.dumps({"mission_id":mission_id})
+        headers = {
+            **self.headers,
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = self.scraper.post(url, headers=headers, data=data)
+        if response.status_code == 400:
+            return None
+        
+        response.raise_for_status()
+        return response.json()
+                
+    def process_query(self, query: str):
+        user_id = self.load_data(query)
+        token = self.user_signin(query, user_id)
+        if not token:
+            return
+        
+        if token:
+            profile = self.user_profile(token)
+            balances = self.user_balance(token)
+            if profile and balances:
+                symbols = ["POINT", "BNB"]
+                balance = {item["symbol"]: item["balance"] for item in balances if item["symbol"] in symbols}
+                point = balance['POINT']
+                bnb = float(balance['BNB']) / 1000000000000000000
+
+                self.log(
+                    f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} {profile['name']} {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}] [ Balance{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} {point} Point {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}-{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} {bnb} BNB {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                )
+                time.sleep(1)
+
+                node = self.node_status(token, user_id)
+                if node:
+                    end_node = node['cycle_ended_at']
+                    if end_node is None:
+                        start = self.node_start(token, user_id)
+                        if start:
+                            end_node_utc = datetime.strptime(start['cycle_ended_at'], '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=timezone.utc)
+                            end_node_wib = end_node_utc.astimezone(wib).strftime('%x %X %Z')
+                            self.log(
+                                f"{Fore.MAGENTA + Style.BRIGHT}[ Node{Style.RESET_ALL}"
+                                f"{Fore.GREEN + Style.BRIGHT} Is Started {Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT}] [ Claim at{Style.RESET_ALL}"
+                                f"{Fore.WHITE + Style.BRIGHT} {end_node_wib} {Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                            )
+                        else:
+                            self.log(
+                                f"{Fore.MAGENTA + Style.BRIGHT}[ Node{Style.RESET_ALL}"
+                                f"{Fore.RED + Style.BRIGHT} Isn't Started {Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                            )
+
+                    else:
+                        now = datetime.now(timezone.utc)
+                        end_node_utc = datetime.strptime(end_node, '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=timezone.utc)
+                        end_node_wib = end_node_utc.astimezone(wib).strftime('%x %X %Z')
+
+                        if now >= end_node_utc:
+                            if bnb >= 0.000063:
+                                claim = self.node_claim(token, user_id)
+                                if claim:
+                                    self.log(
+                                        f"{Fore.MAGENTA + Style.BRIGHT}[ Node{Style.RESET_ALL}"
+                                        f"{Fore.GREEN + Style.BRIGHT} Is Claimed {Style.RESET_ALL}"
+                                        f"{Fore.MAGENTA + Style.BRIGHT}] [ Reward{Style.RESET_ALL}"
+                                        f"{Fore.WHITE + Style.BRIGHT} {claim['total_reward']} {Style.RESET_ALL}"
+                                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                                    )
+                                else:
+                                    self.log(
+                                        f"{Fore.MAGENTA + Style.BRIGHT}[ Node{Style.RESET_ALL}"
+                                        f"{Fore.RED + Style.BRIGHT} Isn't Claimed {Style.RESET_ALL}"
+                                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                                    )
+                            else:
+                                self.log(
+                                    f"{Fore.MAGENTA + Style.BRIGHT}[ Node{Style.RESET_ALL}"
+                                    f"{Fore.YELLOW + Style.BRIGHT} Isn't Claimed {Style.RESET_ALL}"
+                                    f"{Fore.MAGENTA + Style.BRIGHT}] [ Reason{Style.RESET_ALL}"
+                                    f"{Fore.WHITE + Style.BRIGHT} Fees Not Enough {Style.RESET_ALL}"
+                                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                                )
+
+                        else:
+                            self.log(
+                                f"{Fore.MAGENTA + Style.BRIGHT}[ Node{Style.RESET_ALL}"
+                                f"{Fore.YELLOW + Style.BRIGHT} Not Time to Claim {Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT}] [ Claim at{Style.RESET_ALL}"
+                                f"{Fore.WHITE + Style.BRIGHT} {end_node_wib} {Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                            )
+                else:
+                    self.log(
+                        f"{Fore.MAGENTA + Style.BRIGHT}[ Node{Style.RESET_ALL}"
+                        f"{Fore.RED + Style.BRIGHT} Data Is None {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                    )
+                time.sleep(1)
+
+                refferal = self.refferal_info(token)
+                if refferal:
+                    reward = refferal['claimable_amount']
+                    if reward > 0:
+                        claim = self.refferal_claim(token)
+                        if claim:
+                            self.log(
+                                f"{Fore.MAGENTA + Style.BRIGHT}[ Refferal{Style.RESET_ALL}"
+                                f"{Fore.GREEN + Style.BRIGHT} Is Claimed {Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT}] [ Reward{Style.RESET_ALL}"
+                                f"{Fore.WHITE + Style.BRIGHT} {reward} {Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                            )
+                        else:
+                            self.log(
+                                f"{Fore.MAGENTA + Style.BRIGHT}[ Refferal{Style.RESET_ALL}"
+                                f"{Fore.RED + Style.BRIGHT} Isn't Claimed {Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                            )
+                    else:
+                        self.log(
+                            f"{Fore.MAGENTA + Style.BRIGHT}[ Refferal{Style.RESET_ALL}"
+                            f"{Fore.YELLOW + Style.BRIGHT} No Available Reward {Style.RESET_ALL}"
+                            f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                        )
+                else:
+                    self.log(
+                        f"{Fore.MAGENTA + Style.BRIGHT}[ Refferal{Style.RESET_ALL}"
+                        f"{Fore.RED + Style.BRIGHT} Data Is None {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                    )
+                time.sleep(1)
+
+                missions = self.mission_lists(token)
+                if missions:
+                    completed = False
+                    for mission in missions:
+                        mission_id = mission['id']
+                        claimed = mission.get('claimed_at', None)
+
+                        if mission and claimed is None:
+                            claim = self.mission_claim(token, mission_id)
+                            if claim:
+                                self.log(
+                                    f"{Fore.MAGENTA + Style.BRIGHT}[ Task{Style.RESET_ALL}"
+                                    f"{Fore.WHITE + Style.BRIGHT} {mission['name']} {Style.RESET_ALL}"
+                                    f"{Fore.GREEN + Style.BRIGHT}Is Claimed{Style.RESET_ALL}"
+                                    f"{Fore.MAGENTA + Style.BRIGHT} ] [ Reward{Style.RESET_ALL}"
+                                    f"{Fore.WHITE + Style.BRIGHT} {claim['paying_amount']} {Style.RESET_ALL}"
+                                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                                )
+                            else:
+                                self.log(
+                                    f"{Fore.MAGENTA + Style.BRIGHT}[ Task{Style.RESET_ALL}"
+                                    f"{Fore.WHITE + Style.BRIGHT} {mission['name']} {Style.RESET_ALL}"
+                                    f"{Fore.RED + Style.BRIGHT}Isn't Claimed{Style.RESET_ALL}"
+                                    f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}"
+                                )
+                            time.sleep(1)
+                        
+                        else:
+                            completed = True
+
+                    if completed:
+                        self.log(
+                            f"{Fore.MAGENTA + Style.BRIGHT}[ Task{Style.RESET_ALL}"
+                            f"{Fore.GREEN + Style.BRIGHT} Is Completed {Style.RESET_ALL}"
+                            f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                        )
+                else:
+                    self.log(
+                        f"{Fore.MAGENTA + Style.BRIGHT}[ Task{Style.RESET_ALL}"
+                        f"{Fore.RED + Style.BRIGHT} Data Is None {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                    )
+
+            else:
+                self.log(
+                    f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                    f"{Fore.RED + Style.BRIGHT} Data Is None {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                )
+            
+    def main(self):
+        try:
+            with open('query.txt', 'r') as file:
+                queries = [line.strip() for line in file if line.strip()]
+
+            while True:
+                self.clear_terminal()
+                self.welcome()
+                self.log(
+                    f"{Fore.GREEN + Style.BRIGHT}Account's Total: {Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT}{len(queries)}{Style.RESET_ALL}"
+                )
+                self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
+
+                for query in queries:
+                    query = query.strip()
+                    if query:
+                        self.process_query(query)
+                        self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
+                        time.sleep(3)
+
+                seconds = 5
+                while seconds > 0:
+                    formatted_time = self.format_seconds(seconds)
+                    print(
+                        f"{Fore.CYAN+Style.BRIGHT}[ Wait for{Style.RESET_ALL}"
+                        f"{Fore.WHITE+Style.BRIGHT} {formatted_time} {Style.RESET_ALL}"
+                        f"{Fore.CYAN+Style.BRIGHT}... ]{Style.RESET_ALL}",
+                        end="\r"
+                    )
+                    time.sleep(1)
+                    seconds -= 1
+
+        except KeyboardInterrupt:
+            self.log(f"{Fore.RED + Style.BRIGHT}[ EXIT ] MeshChain - BOT{Style.RESET_ALL}")
+        except Exception as e:
+            self.log(f"{Fore.RED + Style.BRIGHT}An error occurred: {e}{Style.RESET_ALL}")
+
+if __name__ == "__main__":
+    bot = MeshChain()
+    bot.main()
